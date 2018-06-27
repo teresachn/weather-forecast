1ed4ddf57717361f3ea52308f82a5700cd803289
teresachn <33697410+teresachn@users.noreply.github.com>
2018-06-28 01:21:51 +0700

    Update, still testing

--- src/AppUI.java
+++ src/AppUI.java
@@ -1,13 +1,10 @@
-import java.awt.BorderLayout;
-import java.awt.Canvas;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.image.BufferStrategy;
-import java.awt.image.BufferedImage;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,36 +12,45 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class AppUI {
-  private JFrame frame;
-  private JPanel panel1;
-  private JButton button;
-  private JTextField text;
-  private JLabel label;
 
   public AppUI() {
-    frame = new JFrame("Weather Forecast");
+    JFrame frame = new JFrame("Weather Forecast");
 
     // SETTING UP PANEL 1
     // AND OBJECT INSIDE IT
-    panel1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    JPanel panel1 = new JPanel();
     panel1.setBounds(0, 0, 260, 480);
     panel1.setBackground(Color.white);
     panel1.setLayout(null);
-    label = new JLabel("Weather Forecast");
-    label.setBounds(10, 20, 250, 40);
-    label.setFont(new Font("Olde English", Font.BOLD, 35));
-    text = new JTextField();
-    text.setBounds(70, 70, 120, 20);
-    button = new JButton("Weather Now");
-    button.setBounds(70, 100, 120, 30);
-    panel1.add(label);
+    JLabel background = new JLabel(new ImageIcon("Asset/Background1.png"));
+    background.setBounds(0, 0, 260, 480);
+    JLabel label1 = new JLabel("Weather");
+    label1.setBounds(52, 40, 210, 50);
+    label1.setFont(new Font("Olde English", Font.BOLD, 50));
+    JLabel label2 = new JLabel("Forecast");
+    label2.setBounds(47, 90, 210, 50);
+    label2.setFont(new Font("Olde English", Font.BOLD, 50));
+    JLabel city = new JLabel("Search city:");
+    city.setBounds(70, 160, 120, 12);
+    city.setFont(new Font("Times New Roman", Font.BOLD, 12));
+    JTextField text = new JTextField();
+    text.setBounds(70, 180, 120, 20);
+    JButton button = new JButton(new ImageIcon("Asset/Button1.png"));
+    button.setBounds(40, 210, 180, 45);
+    button.setBorder(null);
+
+    // add object to panel1
+    panel1.add(background);
+    panel1.add(city);
+    panel1.add(label1);
+    panel1.add(label2);
     panel1.add(text);
     panel1.add(button);
     frame.add(panel1);
 
     // FRAME CONFIGURATION
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(640, 480);
+    frame.setSize(640, 520);
     frame.setLayout(null);
     frame.setVisible(true);
   }
